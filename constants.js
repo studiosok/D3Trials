c4789bb1ac2294b398498eeb20c698213b11ecc8
kristen <studiosok@gmail.com>
2021-02-25 14:57:52 -0500

    initial commit

---
+++ constants.js
@@ -0,0 +1,3 @@
+export const key = "tT5iALYmOedhRlvhcEn2hvamg"
+export const token = "yjzJ6PtgnZMPcwG7ZRZrCexf3BAB-207_Lu7"
+export const url = `https://data.cityofnewyork.us/resource/vfnx-vebw.json`
